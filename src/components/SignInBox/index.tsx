e4b07ce983940118c2f5e9fde6d2e8482bda0099
aluizio neto <aluizioneto03@hotmail.com>
2021-10-23 23:53:18 -0300

    Subindo o codigo

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {  View } from 'react-native';
+
+import { styles } from './styles';
+import {Button} from '../Button'
+import { COLORS } from '../../theme';
+import { useAuth } from '../../Hooks/auth';
+
+export function SignInBox(){
+  const {signIn, isSigningIn} = useAuth(); 
+  return (
+    <View style={styles.container}>
+        <Button title='ENTRAR COM O GITHUB'
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon='github'
+        onPress={signIn}
+        isLoading={isSigningIn}
+        />
+    </View>
+  );
+}
